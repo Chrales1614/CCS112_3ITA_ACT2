d4b89fbd76d5fa977962ecafc32cd028837064e7
Princes Duran <147385394+PrincesDuran@users.noreply.github.com>
2025-03-05 11:41:28 +0800

    cart item

---
+++ src/components/CartItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const CartItem = ({ item, onRemoveFromCart }) => {
+  return (
+    <div>
+      <h4>{item.name}</h4>
+      <p>Price: ₱{item.price} | Quantity: {item.quantity}</p>
+      <button onClick={() => onRemoveFromCart(item.id)}>Remove</button>
+    </div>
+  );
+};
+
+
+export default CartItem;
